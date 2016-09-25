ce68e899f97c38a9297049973aa231832f10c413
mderakhshan <mirderak@gmail.com>
2016-09-25 21:01:45 +0100

    Some additional changes to mke it work with Spring Boot

--- src/main/java/com/example/SpringAngularjsDemoApplication.java
+++ src/main/java/com/example/SpringAngularjsDemoApplication.java
@@ -2,10 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication   // same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class SpringAngularjsDemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAngularjsDemoApplication.class, args);
-	}
+public class SpringAngularjsDemoApplication extends SpringBootServletInitializer {
+
+    private static Class<SpringAngularjsDemoApplication> applicationClass = SpringAngularjsDemoApplication.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 }
